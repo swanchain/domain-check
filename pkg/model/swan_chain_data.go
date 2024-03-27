d4c8c14a793948565ffe0cd260cf0b15879d48a0
James-Chen1 <James.Chen@nbai.io>
2024-03-27 15:50:06 -0400

    removed explorer

--- pkg/model/swan_chain_data.go
+++ pkg/model/swan_chain_data.go
@@ -5,7 +5,6 @@ type SwanChainData struct {
 	WalletAddress string `db:"wallet_address"`
 	Balance       string `db:"balance"`
 	BalanceChange string `db:"balance_change"`
-	Explorer      string `db:"explorer"`
 	NetworkEnv    string `db:"network_env"`
 	UpdatedAt     string `db:"updated_at"`
 }
