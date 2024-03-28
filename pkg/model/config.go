595dd7ae9b30124a9b27bdd4112310b7e92ebe05
James-Chen1 <James.Chen@nbai.io>
2024-03-28 12:13:58 -0400

    fixed config schema

--- pkg/model/config.go
+++ pkg/model/config.go
@@ -1,10 +1,10 @@
 package model
 
 type Config struct {
-	ID       int
-	Key      string
-	Value    string
-	Type     string
-	IsActive bool
-	Note     string
+	ID       int     `db:"id"`
+	Key      string  `db:"key"`
+	Value    string  `db:"value"`
+	Type     string  `db:"type"`
+	IsActive bool    `db:"is_active"`
+	Note     *string `db:"note"`
 }
